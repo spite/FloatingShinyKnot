c5ed6cefb13821fd2423a8315581150482286b2f
thespite <the.spite@gmail.com>
2022-02-15 21:19:00 +0000

    added map custom element

---
+++ map.js
@@ -0,0 +1,142 @@
+import { LitElement, html } from "https://unpkg.com/lit?module";
+
+/* <link
+  rel="stylesheet"
+  href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+  integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+  crossorigin=""
+/>
+<script
+  src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+  integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+  crossorigin=""
+></script> */
+
+class MapBrowser extends LitElement {
+  constructor() {
+    super();
+    this.marker = null;
+  }
+
+  async loadResources() {
+    const cssPromise = new Promise((resolve, reject) => {
+      const css = document.createElement("link");
+      css.setAttribute("rel", "stylesheet");
+      css.setAttribute(
+        "href",
+        "https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+      );
+      css.setAttribute(
+        "integrity",
+        "sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+      );
+      css.setAttribute("crossorigin", "");
+      css.addEventListener("load", (e) => {
+        resolve();
+      });
+      document.head.append(css);
+    });
+
+    const scriptPromise = new Promise((resolve, reject) => {
+      const script = document.createElement("script");
+      script.setAttribute(
+        "src",
+        "https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
+      );
+      script.setAttribute(
+        "integrity",
+        "sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
+      );
+      script.setAttribute("crossorigin", "");
+      script.addEventListener("load", (e) => {
+        resolve();
+      });
+      document.head.append(script);
+    });
+
+    return Promise.all([cssPromise, scriptPromise]);
+  }
+
+  async firstUpdated() {
+    await this.loadResources();
+    const mapDiv = this.shadowRoot.querySelector("#map");
+    this.map = L.map(mapDiv).setView([51.505, -0.09], 13);
+
+    L.tileLayer(
+      "https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}",
+      {
+        attribution:
+          'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: "mapbox/streets-v11",
+        tileSize: 512,
+        zoomOffset: -1,
+        accessToken:
+          "pk.eyJ1Ijoic3BpdGUiLCJhIjoiMU9EUUJlZyJ9.FXckC6N4GTPsqe1ua1u_5g",
+      }
+    ).addTo(this.map);
+
+    this.map.on("click", (e) => this.onMapClick(e));
+  }
+
+  onMapClick(e) {
+    this.addMarker(e.latlng);
+  }
+
+  addMarker(latLng) {
+    if (this.marker) {
+      this.map.removeLayer(this.marker);
+    }
+    this.marker = L.marker(latLng).addTo(this.map);
+    const e = new CustomEvent("map-selection", {
+      bubbles: true,
+      detail: { latLng },
+    });
+    this.dispatchEvent(e);
+  }
+
+  async onChange(e) {
+    const url = `https://nominatim.openstreetmap.org/search?format=json&q=${e.target.value}`;
+    const res = await fetch(url, { mode: "cors" });
+    const data = await res.json();
+    const city = data[0];
+    const bb = city.boundingbox;
+    this.map.fitBounds([
+      [parseFloat(bb[0]), parseFloat(bb[2])],
+      [parseFloat(bb[1]), parseFloat(bb[3])],
+    ]);
+    this.addMarker([parseFloat(city.lat), parseFloat(city.lon)]);
+  }
+
+  render() {
+    return html`
+      <style>
+        :host {
+          display: block;
+          position: relative;
+        }
+        #map {
+          position: absolute;
+          left: 0;
+          top: 0;
+          right: 0;
+          bottom: 2em;
+        }
+        input {
+          position: absolute;
+          bottom: 0;
+          left: 0;
+        }
+      </style>
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css"
+        integrity="sha512-xodZBNTC5n17Xt2atTPuE1HxjVMSvLVW9ocqUKLsCC5CXdbqCmblAshOMAS6/keqq/sMZMZ19scR4PsZChSR7A=="
+        crossorigin=""
+      />
+      <div id="map"></div>
+      <input type="text" id="search" @change=${this.onChange} />
+    `;
+  }
+}
+customElements.define("map-browser", MapBrowser);
